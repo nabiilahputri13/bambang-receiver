8c6f412b304e0e594f3659e15f95f23d8e6368fe
nabiilahputri13 <putrisafa1394@gmail.com>
2024-04-14 01:37:07 +0700

    Create Notification database and Notification repository struct skeleton.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -4,6 +4,8 @@ use lazy_static::lazy_static;
 
 use crate::model::notification::Notification;
 
+
+//Singleton of Database
 lazy_static! {
     static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(vec! []);
 }
